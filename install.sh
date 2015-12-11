c9f3d28dcc2b234e54373e502550cd6764a524c4
John Workmeister <jtworkme@us.ibm.com>
2015-12-11 11:44:17 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "install git, vim and oh-my-zsh first!"
+
+echo "vim stuff"
+mkdir .vim
+mkdir .vim/backup
+mkdir .vim/undos
+mkdir .vim/swap
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+ln aliases ~/.aliases
+ln vimrc ~/.vimrc
+ln tmux ~/.tmux.conf
+
+echo "installing vim plugins"
+vim +PluginInstall +qall
+
