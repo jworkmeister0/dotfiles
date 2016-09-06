aef90b48520e396bbb08ea4fff1c83e256b86b0d
John Workmeister <jtworkme@us.ibm.com>
2016-09-06 11:08:07 -0400

    daily

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo "install git, vim and prezto first!"
+echo "install git, vim, npm, python and prezto first!"
 
-echo "vim stuff"
+echo "making vim dirs"
 mkdir ~/.vim
 mkdir ~/.vim/backup
 mkdir ~/.vim/undos
 mkdir ~/.vim/swap
 
+echo "instlling Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "aliases, vimrc, tmux"
 ln aliases ~/.aliases
 ln vimrc ~/.vimrc
 ln tmux ~/.tmux.conf
+ln jack.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/jack.vim
 
+echo "linking custom prompt"
 ln prompt_super_setup ~/.zprezto/modules/prompt/functions/prompt_super_setup
 
 echo "installing vim plugins"
@@ -22,3 +26,13 @@ vim +PluginInstall +qall
 cd ~/.vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make
+
+cd ~/.vim/bundle/tern_for_vim
+npm install
+
+echo "INSTALLING YouCompleMe"
+echo "YOU NEED THESE PACKAGES:"
+echo "automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel"
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --tern-completer
